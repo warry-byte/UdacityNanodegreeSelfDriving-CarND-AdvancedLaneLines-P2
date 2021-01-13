fd7eead4b67a058679680c8324038e0d9b69e36c
Anthony Antoun <anthony.antoun@sony.com>
2021-01-13 15:53:42 +0100

    ENH: value parameter added to the class as property

--- image_parameter.py
+++ image_parameter.py
@@ -13,15 +13,43 @@ class ImageParameter():
                  min_value=0, 
                  max_value=255, 
                  start_value=255):
-        self.window_name = window_name
-        self.name = parameter_name
-        self.min_value = min_value
-        self.max_value = max_value
+        self.__window_name = window_name
+        self.__name = parameter_name
+        self.__min_value = min_value
+        self.__max_value = max_value
+        self.__value = start_value
         
-        cv2.createTrackbar(self.name, self.window_name, self.min_value, self.max_value, self.update())
+        cv2.createTrackbar(self.name, 
+                           self.window_name, 
+                           self.min_value, 
+                           self.max_value, 
+                           self.update)
         
+    @property
+    def value(self):
+        return self.__value    
+    
+    @value.setter
+    def value(self, val):
+        '''
+        Called when manually setting the position of the trackbar
+
+        Parameters
+        ----------
+        val : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        '''
+        cv2.setTrackbarPos(self.__name, self.__window_name, val)
         
-    def update(self): 
+        # not sure if needed if setTrackbarPos triggers a callback 
+        self.__value = cv2.getTrackbarPos(self.__name, self.__window_name)  # the trackbar object checks for min and max values 
+        
+    def update(self, value): 
         '''
         Callback function called when the trackbar is updated.
 
@@ -30,6 +58,6 @@ class ImageParameter():
         None.
 
         '''
-        
+        self.__value = value  # this is possible because the trackbar values are already constrained
         
         
